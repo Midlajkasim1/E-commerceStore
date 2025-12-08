3bc95eb6bf922beb223cd5b1a710327ee881d115
mithu4747 <midlajmithu16744@gmail.com>
2025-12-08 16:44:53 +0530

    fixed bugs

--- config/db.js
+++ config/db.js
@@ -1,16 +1,22 @@
 const mongoose = require('mongoose');
-const env =require('dotenv').config()
-  
-const connectDB = async ()=>{
-    try {
-      await mongoose.connect(process.env.MONGODB_URI)  
-      console.log("DB is connected");
-      
-    } catch (error) {
-        console.log("DB connection is error",error.message);
-        process.exit(1)
-        
-    }
-}
+
+let isConnected = false; 
+const connectDB = async () => {
+  if (isConnected) {
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI, {
+      serverSelectionTimeoutMS: 5000
+    });
+
+    isConnected = db.connections[0].readyState === 1;
+    console.log("MongoDB connected (Vercel serverless)");
+  } 
+  catch (error) {
+    console.error("MongoDB connection error:", error.message);
+  }
+};
 
 module.exports = connectDB;
